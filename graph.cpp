096e3bba51f9062e56f5317f666a6813bcf20e84
Rodrigo Sánchez <62879165+rsanchezm98@users.noreply.github.com>
2021-09-23 20:55:37 +0200

    update

--- graph.cpp
+++ graph.cpp
@@ -1,61 +1,74 @@
-#include <list>
-#include <vector>
 #include "graph.hpp"
 
-Graph::Graph(const size_t size_of_graph)
-  : m_size_of_graph(size_of_graph)
-  {
-    // initialize the size of the vector
-    for(size_t i = 0; i < m_size_of_graph; i++)
-    {
-      std::list<size_t> empty_list;
-      m_graph.push_back(empty_list);
-    }
-  }
-  
-void Graph::AddEdges(size_t origin, size_t end)
+namespace graph
 {
-  // add the end vertex to the origin vertex
-  m_graph[origin].push_back(end);
-}
 
-void Graph::PrintGraph()
-{
-  for(size_t i = 0; i < m_size_of_graph; i++)
-  {
-    std::cout << i ": {";
-    for(size_t j = 0; j < m_graph[i].size(); j++)
+  Graph::Graph(const size_t size_of_graph)
+    : m_size_of_graph(size_of_graph)
     {
-      if(j == m_graph[i].size() - 1)
+      // initialize the size of the vector
+      for(size_t i = 0; i < m_size_of_graph; i++)
       {
-        std::cout << m_graph[i][j];
-      }else
-      {
-        std::cout << m_graph[i][j] << ", ";
+        std::vector<size_t> empty_vector;
+        m_graph.push_back(empty_vector);
       }
     }
-    std::cout << "}\n";
-  }
-}
-
-void Graph::SolveGraph(const std::string& mode, const size_t origin)
-{
-  if(mode == "BFS")
+    
+  void Graph::AddEdges(size_t origin, size_t end)
   {
-    BSF(m_graph, origin);
+    // add the end vertex to the origin vertex
+    m_graph[origin].push_back(end);
   }
-  
-  if(mode == "DFS")
-  {
-    DFS(m_graph, origin);
-  }
-}
 
-std::vector<std::list<size_t>> Graph::GetGraph()
-{
-  return m_graph;
+  void Graph::PrintGraph()
+  {
+    for(size_t i = 0; i < m_size_of_graph; i++)
+    {
+      std::cout << i << ": {";
+      for(size_t j = 0; j < m_graph[i].size(); j++)
+      {
+        if(j == m_graph[i].size() - 1)
+        {
+          std::cout << m_graph[i][j];
+        }else
+        {
+          std::cout << m_graph[i][j] << ", ";
+        }
+      }
+      std::cout << "}\n";
+    }
+  }
+
+  void Graph::SolveGraph(const std::string& mode, const size_t origin)
+  {
+    if(mode == "BFS")
+    {
+      BFS(m_graph, origin);
+    }
+    
+    if(mode == "DFS")
+    {
+      DFS(m_graph, origin);
+    }
+  }
+
+  bool Graph::BFS(const std::vector<std::vector<size_t>>& graph, const size_t origin)
+  {
+    
+    return true;
+  }
+
+  bool Graph::DFS(const std::vector<std::vector<size_t>>& graph, const size_t origin)
+  {
+    return true;
+  }
+
+  std::vector<std::vector<size_t>> Graph::GetGraph()
+  {
+    return m_graph;
+  }
+
 }
-  
 
 
 
