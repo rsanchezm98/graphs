be465bf50b7a5c7bcaf47c2d213882f987c68392
Rodrigo Sánchez <62879165+rsanchezm98@users.noreply.github.com>
2021-09-23 20:56:04 +0200

    update

--- graph.hpp
+++ graph.hpp
@@ -1,1 +1,38 @@
+#include <vector>
+#include <iostream>
 
+namespace graph
+{
+    enum class state{WHITE, GREY, BLACK};
+
+    class Graph
+    {
+        public:
+
+            // Constructor
+            Graph(const size_t size_of_graph);
+
+            // AddEdges
+            void AddEdges(size_t origin, size_t end);
+
+            // PrintGraph
+            void PrintGraph();
+
+            // SolveGraph
+            void SolveGraph(const std::string& mode, const size_t origin);
+
+            // GetGraph
+            std::vector<std::vector<size_t>> GetGraph();
+
+            // BFS
+            bool BFS(const std::vector<std::vector<size_t>>& graph, const size_t origin);
+
+            //D FS
+            bool DFS(const std::vector<std::vector<size_t>>& graph, const size_t origin);
+
+        private:
+
+            size_t m_size_of_graph;
+            std::vector<std::vector<size_t>> m_graph;
+    }
+}
--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-  Graph graph(2);
+  graph::Graph graph(2);
   graph.AddEdges(0,1);
   graph.PrintGraph();
   graph.SolveGraph("BFS", 0);
